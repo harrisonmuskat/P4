68096c7dba4edd5f317377664c3a8b7a25f49d8d
Harrison Muskat <harrisonmuskat@gmail.com>
2014-08-07 12:08:34 -0400

    Still working on production DB

--- app/routes.php
+++ app/routes.php
@@ -47,7 +47,7 @@ Route::get('/deleteitem', 'PantryController@deleteItem');
 Route::get('/deleteRecipe', 'RecipeController@deleteRecipe');
 
 //This is the route to seed the production database (admin protected)
-Route::get('/seed', array('before' => array('auth'|'admin'), 'uses' => 'AdminController@loadDatabase'));
+//Route::get('/seed', array('before' => array('auth'|'admin'), 'uses' => 'AdminController@loadDatabase'));
 
 /*
 -----------------------
@@ -120,4 +120,47 @@ Route::get('mysql-test', function() {
 });
 */
 
+Route::get('/debug', function() {
 
+    echo '<pre>';
+
+    echo '<h1>environment.php</h1>';
+    $path   = base_path().'/environment.php';
+
+    try {
+        $contents = 'Contents: '.File::getRequire($path);
+        $exists = 'Yes';
+    }
+    catch (Exception $e) {
+        $exists = 'No. Defaulting to `production`';
+        $contents = '';
+    }
+
+    echo "Checking for: ".$path.'<br>';
+    echo 'Exists: '.$exists.'<br>';
+    echo $contents;
+    echo '<br>';
+
+    echo '<h1>Environment</h1>';
+    echo App::environment().'</h1>';
+
+    echo '<h1>Debugging?</h1>';
+    if(Config::get('app.debug')) echo "Yes"; else echo "No";
+
+    echo '<h1>Database Config</h1>';
+    print_r(Config::get('database.connections.mysql'));
+
+    echo '<h1>Test Database Connection</h1>';
+    try {
+        $results = DB::select('SHOW DATABASES;');
+        echo '<strong style="background-color:green; padding:5px;">Connection confirmed</strong>';
+        echo "<br><br>Your Databases:<br><br>";
+        print_r($results);
+    } 
+    catch (Exception $e) {
+        echo '<strong style="background-color:crimson; padding:5px;">Caught exception: ', $e->getMessage(), "</strong>\n";
+    }
+
+    echo '</pre>';
+
+});
