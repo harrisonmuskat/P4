5e0e8a50aeb0db3fd63ccbd60cca5c92bb5cadeb
Harrison Muskat <harrisonmuskat@gmail.com>
2014-08-06 20:01:14 -0400

    Prepared live database

---
+++ app/config/production/database.php
@@ -0,0 +1,17 @@
+<?php
+
+return array(
+
+    'connections' => array(
+
+        'mysql' => array(
+            'driver'    => 'mysql',
+            'host'      => 'tunnel.pagodabox.com:3306',
+            'database'  => 'pantry',
+            'username'  => 'jessia',
+            'password'  => 'PfE1ZoaS',
+        ),
+
+    ),
+
+);
